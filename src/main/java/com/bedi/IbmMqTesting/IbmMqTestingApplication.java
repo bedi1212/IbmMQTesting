0016a0b22206ab925e1c9f077a2157d1cac421cf
bedi <bedi.9494@gmail.com>
2022-03-01 15:30:59 -0500

    IBM mQ testing

---
+++ src/main/java/com/bedi/IbmMqTesting/IbmMqTestingApplication.java
@@ -0,0 +1,13 @@
+package com.bedi.IbmMqTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmMqTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmMqTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bedi/IbmMqTesting/IbmMqTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedi.IbmMqTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmMqTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
